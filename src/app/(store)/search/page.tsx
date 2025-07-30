126bc7951ce57b9be8492a8e0a4ff6f3bc0832c2
Caio Luna <ccluna@gmail.com>
2025-07-29 22:18:54 -0300

    feat: layout search page

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,4 +1,38 @@
+import {FeaturedLink} from "@/components/FeaturedLink";
+
 export default async function SearchPage() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return <div>SearchPage</div>;
+  return (
+    <div className='flex flex-col gap-4'>
+      <p className='text-sm'>
+        Resultados para: <span className='font-semibold'>moleton</span>
+      </p>
+
+      <div className='grid grid-cols-3 gap-6'>
+        <FeaturedLink
+          key={1}
+          href={'moletom-never-stop-learning'}
+          imageUrl={'/moletom-never-stop-learning.png'}
+          label={'Moletom Never Stop Learning'}
+          price={129}
+          size={460}
+        />
+        <FeaturedLink
+          key={1}
+          href={'moletom-never-stop-learning'}
+          imageUrl={'/moletom-never-stop-learning.png'}
+          label={'Moletom Never Stop Learning'}
+          price={129}
+          size={460}
+        />
+        <FeaturedLink
+          key={1}
+          href={'moletom-never-stop-learning'}
+          imageUrl={'/moletom-never-stop-learning.png'}
+          label={'Moletom Never Stop Learning'}
+          price={129}
+          size={460}
+        />
+      </div>
+    </div>
+  )
 }
