2ec4889915c2a9a6b27767d9aa329fd9c674e4db
Caio Luna <ccluna@gmail.com>
2025-08-02 22:48:22 -0300

    feat: readme

--- src/app/(store)/search/constants.tsx
+++ src/app/(store)/search/constants.tsx
@@ -3,9 +3,9 @@ import {api} from "@/data/api";
 
 export async function searchProducts(query: string): Promise<Product[]> {
   const response = await api(`/products/search?q=${query}`, {
-    // next: {
-    //   revalidate: 60 * 60,
-    // },
+    next: {
+      revalidate: 60 * 60,
+    },
   });
 
   return await response.json();
